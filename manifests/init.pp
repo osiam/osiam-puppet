7b49c2768136483c990e563d72770b3aa179868a
Philipp Eder <p.eder@tarent.de>
2013-03-20 15:33:12 +0100

    removed package and unnecessary ''

--- manifests/init.pp
+++ manifests/init.pp
@@ -64,9 +64,8 @@ class osiam (
       file { 'db-config.properties':
         path    => "${homedir}/db-config.properties",
         ensure  => $osiam::ensure,
-        require => Package['$tomcatservice'],
         content => template('osiam/db-config.properties.erb'),
-        notify => Service['$tomcatservice']
+        notify => Service[$tomcatservice]
       }
 
     case $ensure {
