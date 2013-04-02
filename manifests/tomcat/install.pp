f523e9ebc2ee7b20477b906717739abe38130cec
Kevin Viola Schmitz <k.schmitz@tarent.de>
2013-04-02 11:33:56 +0200

    Make tomcat install removeable

--- manifests/tomcat/install.pp
+++ manifests/tomcat/install.pp
@@ -22,37 +22,33 @@
 class osiam::tomcat::install {
     if ( $::operatingsystem == 'CentOS' ) or ( $::lsbmajdistrelease == '6') {
         file { '/etc/yum.repos.d/jpackage.repo':
-            ensure => present,
+            ensure => $osiam::ensure,
             owner  => 'root',
             group  => 'root',
             mode   => '0644',
             source => 'puppet:///modules/osiam/jpackage.repo',
-            notify => [
-                Exec['jpackage-repo-yumcleanall'],
-                Exec['jpackage-gpg'],
-            ],
         }
 
-        exec { 'jpackage-repo-yumcleanall':
-            path        => '/usr/bin',
-            command     => 'yum clean all',
-            refreshonly => true,
-            require     => File['/etc/yum.repos.d/jpackage.repo'],
-        }
+        if $osiam::ensure == 'present' {
+            exec { 'jpackage-repo-yumcleanall':
+                path        => '/usr/bin',
+                command     => 'yum clean all',
+                refreshonly => true,
+                before      => Package['tomcat7'],
+                subscribe   => File['/etc/yum.repos.d/jpackage.repo'],
+            }
 
-        exec { 'jpackage-gpg':
-            path        => '/bin',
-            command     => 'rpm --import http://www.jpackage.org/jpackage.asc',
-            refreshonly => true,
-            require     => File['/etc/yum.repos.d/jpackage.repo'],
+            exec { 'jpackage-gpg':
+                path        => '/bin',
+                command     => 'rpm --import http://www.jpackage.org/jpackage.asc',
+                refreshonly => true,
+                before      => Package['tomcat7'],
+                subscribe   => File['/etc/yum.repos.d/jpackage.repo'],
+            }
         }
 
         package { 'tomcat7':
-            ensure  => present,
-            require => [
-                Exec['jpackage-repo-yumcleanall'],
-                Exec['jpackage-gpg'],
-            ],
+            ensure => $osiam::ensure,
         }
 
         firewall { '099 tomcat':
