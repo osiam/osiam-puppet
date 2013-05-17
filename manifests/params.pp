a6b784cc646d8fdac4033232520e5b7ab1a27bfb
Kevin Viola Schmitz <k.schmitz@tarent.de>
2013-05-17 11:48:41 +0200

    Add support for multiple operatingsystems into params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -20,12 +20,21 @@
 #   Kevin Viola Schmitz <k.schmitz@tarent.de>
 #
 class osiam::params {
-    $package            = 'postgresql92-server'
-    $service            = 'postgresql-9.2'
-    $cpath              = '/var/lib/pgsql/9.2/data'
-    $rpm                = 'pgdg-redhat92-9.2-7.noarch.rpm'
-    $repository         = "http://yum.postgresql.org/9.2/redhat/rhel-${::lsbmajdistrelease}-x86_64/${rpm}"
-    $repositorytmp      = "/tmp/${rpm}"
-    $listenaddresses    = '0.0.0.0'
-    $port               = '5432'
+    case $::operatingsystem {
+        'CentOS': {
+            $package            = 'postgresql92-server'
+            $service            = 'postgresql-9.2'
+            $cpath              = '/var/lib/pgsql/9.2/data'
+            $rpm                = 'pgdg-redhat92-9.2-7.noarch.rpm'
+            $repository         = "http://yum.postgresql.org/9.2/redhat/rhel-${::lsbmajdistrelease}-x86_64/${rpm}"
+            $repositorytmp      = "/tmp/${rpm}"
+            $listenaddresses    = '0.0.0.0'
+            $port               = '5432'
+        }
+        default: {
+            fail("Unsupported operatingsystem: ${::operatingsystem}")
+        }
+    }
+    
+    
 }
