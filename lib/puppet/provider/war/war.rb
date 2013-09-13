46c449b108ada2f8281aa16083171e6aeb147c24
Kevin Viola Schmitz <k.schmitz@tarent.de>
2013-09-13 08:32:47 +0200

    Replace tabs with whitespaces

--- lib/puppet/provider/war/war.rb
+++ lib/puppet/provider/war/war.rb
@@ -5,129 +5,131 @@ require 'fileutils'
 require 'etc'
 
 Puppet::Type.type(:war).provide(:war) do
-	desc "War provider."
-	include Puppet::Util::Execution
-	include Puppet::Util::Warnings
+    desc "War provider."
+    include Puppet::Util::Execution
+    include Puppet::Util::Warnings
 
-	def artifact
-		@resource[:path] + '/' + @resource[:artifactid] + '.war'
-	end
+    def artifact
+        @resource[:path] + '/' + @resource[:artifactid] + '.war'
+    end
 
-	# Function to get file owner.
-	def owner
-		uid = File.stat(self.artifact).uid
-		Etc.getpwuid(uid).name
-	end
-	# Function to enforce file owner.
-	def owner=(owner)
-		owner = @resource[:owner]
+    # Function to get file owner.
+    def owner
+        uid = File.stat(self.artifact).uid
+        Etc.getpwuid(uid).name
+    end
+    # Function to enforce file owner.
+    def owner=(owner)
+        owner = @resource[:owner]
 
-		begin
-			File.chown(Etc.getpwnam(owner).uid,nil,self.artifact)
-		rescue => detail
-			raise Puppet::Error, "Failed to set owner to '#{owner}': #{detail}"
-		end
-	end
-	# Function to get file group.
-	def group
-		gid = File.stat(self.artifact).gid
-		Etc.getgrgid(gid).name
-	end
-	# Function to enforce file group.
-	def group=(group)
-		group = @resource[:group]
+        begin
+            File.chown(Etc.getpwnam(owner).uid,nil,self.artifact)
+        rescue => detail
+            raise Puppet::Error, "Failed to set owner to '#{owner}': #{detail}"
+        end
+    end
 
-		begin
-			File.chown(nil,Etc.getgrnam(group).gid,self.artifact)
-		rescue => detail
-			raise Puppet::Error, "Failed to set group to '#{group}': #{detail}"
-		end
-	end
+    # Function to get file group.
+    def group
+        gid = File.stat(self.artifact).gid
+        Etc.getgrgid(gid).name
+    end
 
-	# Function to create file.
-	# Will be called if ensure is set to 'present'.
-	def create
-		version		= @resource[:version]
-		artifactid	= @resource[:artifactid]
-		path		= @resource[:path]
-		owner		= @resource[:owner]
-		owner		= owner.nil? || owner.empty? ? "root" : owner
-		group		= @resource[:group]
-		group		= group.nil? || group.empty? ? "root" : group
-		groupid		= 'org.osiam'
-		plugin		= "2.4"
+    # Function to enforce file group.
+    def group=(group)
+        group = @resource[:group]
 
-		if version =~ /^.*-SNAPSHOT$/
-			repository = 'http://maven-repo.evolvis.org/snapshots'
-		else
-			repository = 'http://maven-repo.evolvis.org/release'
-		end
+        begin
+            File.chown(nil,Etc.getgrnam(group).gid,self.artifact)
+        rescue => detail
+            raise Puppet::Error, "Failed to set group to '#{group}': #{detail}"
+        end
+    end
 
-		# Set maven parameters
-		mvn = "org.apache.maven.plugins:maven-dependency-plugin:#{plugin}:get -Dpackaging=war"
-		mvn = mvn + " -DgroupId=#{groupid} -DartifactId=#{artifactid} -Dversion=#{version}"
-		mvn = mvn + " -DremoteRepositories=#{repository} -Ddest='" + self.artifact + "'"
-		
-		# Execute maven and download artifact
-		command = ["mvn #{mvn}"]
-		output, status	= Puppet::Util::SUIDManager.run_and_capture(command, 'root', 'root')
-		debug output if status.exitstatus == 0
-		debug "Exit Status = #{status.exitstatus}"
-		if status.exitstatus != 0
-			raise Puppet::Error, "Failed to download artifact: #{output}"
-		end
-		# Change artifact permission
-		begin
-			File.chown(Etc.getpwnam(owner).uid,Etc.getgrnam(group).gid,self.artifact)
-		rescue => detail
-			raise Puppet::Error, "Failed to set group to '#{group}': #{detail}"
-		end
-	end
+    # Function to create file.
+    # Will be called if ensure is set to 'present'.
+    def create
+        version     = @resource[:version]
+        artifactid  = @resource[:artifactid]
+        path        = @resource[:path]
+        owner       = @resource[:owner]
+        owner       = owner.nil? || owner.empty? ? "root" : owner
+        group       = @resource[:group]
+        group       = group.nil? || group.empty? ? "root" : group
+        groupid     = 'org.osiam'
+        plugin      = "2.4"
 
-	# Function to delete file.
-	# Will be called if ensure is set to 'absent'.
-	def destroy
-		File.delete(self.artifact)
-	end
+        if version =~ /^.*-SNAPSHOT$/
+            repository = 'http://maven-repo.evolvis.org/snapshots'
+        else
+            repository = 'http://maven-repo.evolvis.org/release'
+        end
 
-	# Function to check if file exists.
-	# In this case it will also check its md5sum and
-	# compare it to the newest artifact
-	# (of the same version) in the repository.
-	def exists?
-		version		= @resource[:version]
-		artifactid	= @resource[:artifactid]
-		path 		= @resource[:path]
-		groupid		= 'org/osiam'
-		repository 	= 'http://maven-repo.evolvis.org'
+        # Set maven parameters
+        mvn = "org.apache.maven.plugins:maven-dependency-plugin:#{plugin}:get -Dpackaging=war"
+        mvn = mvn + " -DgroupId=#{groupid} -DartifactId=#{artifactid} -Dversion=#{version}"
+        mvn = mvn + " -DremoteRepositories=#{repository} -Ddest='" + self.artifact + "'"
 
-		repoappend = version =~ /^.*-SNAPSHOT$/ ? 'snapshots' : 'release'
-		repository = repository + '/' + repoappend
-		url = "#{repository}/#{groupid}/#{artifactid}/#{version}"
+        # Execute maven and download artifact
+        command = ["mvn #{mvn}"]
+        output, status	= Puppet::Util::SUIDManager.run_and_capture(command, 'root', 'root')
+        debug output if status.exitstatus == 0
+        debug "Exit Status = #{status.exitstatus}"
+        if status.exitstatus != 0
+            raise Puppet::Error, "Failed to download artifact: #{output}"
+        end
+        # Change artifact permission
+        begin
+            File.chown(Etc.getpwnam(owner).uid,Etc.getgrnam(group).gid,self.artifact)
+        rescue => detail
+            raise Puppet::Error, "Failed to set group to '#{group}': #{detail}"
+        end
+    end
+
+    # Function to delete file.
+    # Will be called if ensure is set to 'absent'.
+    def destroy
+        File.delete(self.artifact)
+    end
+
+    # Function to check if file exists.
+    # In this case it will also check its md5sum and
+    # compare it to the newest artifact
+    # (of the same version) in the repository.
+    def exists?
+        version     = @resource[:version]
+        artifactid  = @resource[:artifactid]
+        path        = @resource[:path]
+        groupid     = 'org/osiam'
+        repository  = 'http://maven-repo.evolvis.org'
+
+        repoappend = version =~ /^.*-SNAPSHOT$/ ? 'snapshots' : 'release'
+        repository = repository + '/' + repoappend
+        url = "#{repository}/#{groupid}/#{artifactid}/#{version}"
 
 
-		if File.exists?("#{path}/#{artifactid}.war")
-			# Get our artifacts md5sum
-			warmd5 = Digest::MD5.hexdigest(File.read(self.artifact))
+        if File.exists?("#{path}/#{artifactid}.war")
+            # Get our artifacts md5sum
+            warmd5 = Digest::MD5.hexdigest(File.read(self.artifact))
 
-			# Get newest artifacts md5sum (from repo)
-			# Download the index of #{repository}
-			command = [ "wget -O- #{url} 2>&1 | grep '.war.md5' | tail -n 1" ]
-			md5file, status = Puppet::Util::SUIDManager.run_and_capture(command, 'root', 'root')
-			debug command if status.exitstatus == 0
-			debug "Exit Status = #{status.exitstatus}"
-			# Extract the newest wars .md5file file name
-			md5file = md5file.sub(/^.*<a href="([^"]*)">.*$/, '\1')
-			# Finaly download the md5 sum
-			command2 = [ "wget -qO- #{url}/#{md5file}" ]
-			md5sum, status	= Puppet::Util::SUIDManager.run_and_capture(command2, 'root', 'root')
-			debug md5sum if status.exitstatus == 0
-			debug "Exit Status = #{status.exitstatus}"
+            # Get newest artifacts md5sum (from repo)
+            # Download the index of #{repository}
+            command = [ "wget -O- #{url} 2>&1 | grep '.war.md5' | tail -n 1" ]
+            md5file, status = Puppet::Util::SUIDManager.run_and_capture(command, 'root', 'root')
+            debug command if status.exitstatus == 0
+            debug "Exit Status = #{status.exitstatus}"
+            # Extract the newest wars .md5file file name
+            md5file = md5file.sub(/^.*<a href="([^"]*)">.*$/, '\1')
+            # Finaly download the md5 sum
+            command2 = [ "wget -qO- #{url}/#{md5file}" ]
+            md5sum, status	= Puppet::Util::SUIDManager.run_and_capture(command2, 'root', 'root')
+            debug md5sum if status.exitstatus == 0
+            debug "Exit Status = #{status.exitstatus}"
 
-			# return true if md5sum == warmd5
-			return md5sum == warmd5 ? true : false
-		else
-			return false
-		end
-	end
+            # return true if md5sum == warmd5
+            return md5sum == warmd5 ? true : false
+        else
+            return false
+        end
+    end
 end
--- lib/puppet/type/war.rb
+++ lib/puppet/type/war.rb
@@ -1,45 +1,45 @@
 require 'puppet/type'
 
 Puppet::Type.newtype(:war) do
-	@doc = "War download"
+    @doc = "War download"
 
-	# Make this type accept "ensure => present" and "ensure => absent".
-	# present will call providers function 'exists?' and 'create'.
-	# absent will call providers function 'exists?' and 'destroy'.
-	# default value is 'present'.
-	ensurable do
-		self.defaultvalues
-		defaultto :present
-	end
+    # Make this type accept "ensure => present" and "ensure => absent".
+    # present will call providers function 'exists?' and 'create'.
+    # absent will call providers function 'exists?' and 'destroy'.
+    # default value is 'present'.
+    ensurable do
+        self.defaultvalues
+        defaultto :present
+    end
 
-	def self.title_patterns
-		[ [ /^(.*?)\/*\Z/m, [ [ :artifactid, lambda{|x| x} ] ] ] ]
-	end
+    def self.title_patterns
+        [ [ /^(.*?)\/*\Z/m, [ [ :artifactid, lambda{|x| x} ] ] ] ]
+    end
 
-	# Create parameters for this type. They will be used by the provider
-	newparam(:artifactid) do
-		desc "War artifact name."
-		isnamevar
-	end
-	newparam(:version) do
-		desc "War artifact version."
-	end
-	newparam(:path) do
-		desc "Location where the war file will be saved."
-	end
+    # Create parameters for this type. They will be used by the provider
+    newparam(:artifactid) do
+        desc "War artifact name."
+        isnamevar
+    end
+    newparam(:version) do
+        desc "War artifact version."
+    end
+    newparam(:path) do
+        desc "Location where the war file will be saved."
+    end
 
-	# Create properties. These will call a provider function to check
-	# for a value and another function to enforce it.
-	#
-	# owner for example will call function "owner" which will check
-	# for ownership of the file that is being deploying with this type.
-	# If the file is not owner by whom it should be, the function
-	# owner=(owner) will be called to set things straight.
-	newproperty(:owner) do
-		desc "Owner that will be set for the artifact."
-	end
-	newproperty(:group) do
-		desc "Group that will be set for the artifact."
-	end
+    # Create properties. These will call a provider function to check
+    # for a value and another function to enforce it.
+    #
+    # owner for example will call function "owner" which will check
+    # for ownership of the file that is being deploying with this type.
+    # If the file is not owner by whom it should be, the function
+    # owner=(owner) will be called to set things straight.
+    newproperty(:owner) do
+        desc "Owner that will be set for the artifact."
+    end
+    newproperty(:group) do
+        desc "Group that will be set for the artifact."
+    end
 
 end
